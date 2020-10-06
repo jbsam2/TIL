88f6f8e82402e5e4ba97bea21cad7ceda7f79fa2
KGB <kbsam2@gmail.com>
2020-10-06 09:22:44 +0900

    1006 study

---
+++ 12주차/20201006/articles/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import get_object_or_404
+from .models import Article, Comment
+from .serializers import ArticleSerializer, ArticleListSerializer, CommentSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+# Create your views here.
+@api_view(['GET','POST'])
+def article_list_create(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
+
+@api_view(['GET','DELETE','PUT'])
+def article_detail_update_delete(request, article_pk):
+    article = get_object_or_404(Article,pk=article_pk)
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response({'id': article_pk},status=status.HTTP_204_NO_CONTENT)
+    else:
+        serializer = ArticleSerializer(article,data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def create_comment(request, article_pk):
+    article = get_object_or_404(Article,pk=article_pk)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(article=article)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def comment_list(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def comment_detail_update_delete(request, comment_pk):
+    comment = get_object_or_404(Comment,pk=comment_pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response({'id':comment_pk}, status=status.HTTP_204_NO_CONTENT)
+    else:
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
