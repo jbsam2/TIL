016c3f149d667e2c48f88af0b813ef8897b69843
KGB <kbsam2@gmail.com>
2020-11-12 13:33:15 +0900

    1112

---
+++ 17주차/20201112/practice/project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h2>Cat Image</h2>
+    <p><img :src="caturl" alt="" width="50%"></p>
+    <button @click="geturl">Get Cat</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const API_URL = 'https://api.thecatapi.com/v1/images/search'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      caturl: '',
+    }
+  },
+  methods: {
+    geturl(){
+      axios.get(API_URL)
+      .then((res)=>{
+        this.caturl = res.data[0].url
+      })
+    }
+  }
+}
+</script>
--- 17주차/20201112/workshop/project/src/App.vue
+++ 17주차/20201112/workshop/project/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="container">
     <h1>My First Youtube Project</h1>
-    <!-- 3.보여준다. -->
     <SearchBar @input-change="onInputChange" />
-    <VideoDetail :video='selectedVideo'/>
     <VideoList :videos='videos' @select-video='onVideoSelect'/>
+    <VideoDetail :video='selectedVideo'/>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 
-//1. 불러온다.
 import SearchBar from '@/components/SearchBar'
 import VideoList from '@/components/VideoList'
 import VideoDetail from '@/components/VideoDetail'
 
-//'AIzaSyCsh655apyg5BX0yuWowxu0gZRrIeqc2_I'
-const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
+const API_KEY = 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 
 export default {
   name: 'App',
   components: {
-    // ES6 Object 축약 문법
-    // SearchBar: SearchBar, 
-    //2. 등록한다.
     SearchBar,
     VideoList,
     VideoDetail,
@@ -40,13 +34,8 @@ export default {
   },
   methods: {
     onInputChange: function (inputText) {
-      console.log('데이터가 SearchBar로부터 올라왔다!!!!')
-      console.log(inputText)
-
-      //1. 자식 컴포넌트(SearchBar.vue)로부터 올라온 데이터 inputValue라는 App.vue 컴포넌트 데이터에 넣는다.
       this.inputValue = inputText
 
-      //2. Youtube API로 요청을 위한 준비를 한다.
       const params = {
         key: API_KEY,
         part: 'snippet',
@@ -54,15 +43,10 @@ export default {
         q: this.inputValue,
       }
 
-      //3. Youtube로 axios를 활용해 요청을 보낸다.
       axios.get(API_URL, {
         params,
       })
       .then((res) => {
-        console.log(res)
-        console.log(res.data.items)
-      
-        //4. Youtube로부터 응답 받은 영상 목록(배열)을 배열에 넣는다.
         this.videos = res.data.items
       })
       .catch((err) => {
